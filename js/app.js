92ffc250f90e60a5df95eb768c03ff1e6217cf95
Juan Miguel Imaz <imazjm@adimedia.net>
2021-04-26 13:45:37 +0200

    Más pruebas

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function() {
+//     navigator.serviceWorker.register('/sw.js')
+//       .then(function() {
+//         console.log('ServiceWorker registered!');
+//     })
+//     .catch(function(err) {
+//       console.log('ServiceWorker failed :(', err);
+//     });
+//   });
+// }
+
+
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js')
-      .then(function() {
-        console.log('ServiceWorker registered!');
-    })
-    .catch(function(err) {
-      console.log('ServiceWorker failed :(', err);
-    });
-  });
+  navigator.serviceWorker.register('/sw.js');
 }
