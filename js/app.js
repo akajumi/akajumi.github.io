42910b1cf59dff44b4df86eda49b6aa04de0623c
Juan Miguel Imaz <imazjm@adimedia.net>
2021-04-28 17:44:04 +0200

    Prueba de página de inicio de la PWA instlada distinta de home.html. Incluye pagana sin conexión a internet.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,11 @@
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', function() {
-//     navigator.serviceWorker.register('/sw.js')
-//       .then(function() {
-//         console.log('ServiceWorker registered!');
-//     })
-//     .catch(function(err) {
-//       console.log('ServiceWorker failed :(', err);
-//     });
-//   });
-// }
-
-
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js');
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js')
+      .then(function() {
+        console.log('ServiceWorker registered!');
+    })
+    .catch(function(err) {
+      console.log('ServiceWorker failed :(', err);
+    });
+  });
 }
--- sw.js
+++ sw.js
@@ -21,20 +21,6 @@ const CACHE_NAME = "offline";
 // Customize this with a different URL if needed.
 const OFFLINE_URL = "offline.html";
 
-let deferredPrompt;
-
-self.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
-  // Optionally, send analytics event that PWA install promo was shown.
-  console.log(`'beforeinstallprompt' event was fired.`);
-});
-
-
 self.addEventListener("install", (event) => {
   event.waitUntil(
     (async () => {
